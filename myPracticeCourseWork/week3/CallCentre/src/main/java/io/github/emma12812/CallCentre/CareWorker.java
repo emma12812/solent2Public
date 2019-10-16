f5ec6c2808f69b27016bec9b8ce98a7d751cbb01
emma12812 <emma12812@gmail.com>
2019-10-16 09:03:23 +0100

    week 3

---
+++ myPracticeCourseWork/week3/CallCentre/src/main/java/io/github/emma12812/CallCentre/CareWorker.java
@@ -0,0 +1,10 @@
+package io.github.emma12812.CallCentre;
+
+import java.util.Map;
+
+public class CareWorker {
+    private String name;
+    int workerId;
+    double phoneNumber;
+    Map<Coords,CareWorker> workerCoords;
+}
---
+++ myPracticeCourseWork/week3/CallCentre/src/main/java/io/github/emma12812/CallCentre/Main.java
@@ -0,0 +1,4 @@
+package io.github.emma12812.CallCentre;
+
+public class Main {
+}
